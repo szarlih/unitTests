ea67372018e31235f34198594e1a1804512dc9f1
szarlih <39311561+szarlih@users.noreply.github.com>
2019-03-22 10:01:16 +0100

    after workshops

---
+++ Program.cs
@@ -0,0 +1,110 @@
+﻿namespace UnitTestWorkshop
+{
+    using System;
+    using UnitTestWorkshop.Board;
+    using UnitTestWorkshop.MajorChessPieces;
+
+    class Program
+    {
+        private static string input;
+        private static IChessPiece selected;
+
+        static void Main(string[] args)
+        {
+            ChessBoard board = new ChessBoard();
+            Demo(board);
+            ParticipantWork();
+
+            ProgramLoop(board);
+        }
+
+        private static void Demo(IChessBoard board)
+        {
+            board.SetPiece(new Tuple<int, int>(0, 7), new Knight(Utils.ColorsEnum.Black, new Tuple<int, int>(0, 7)));
+            board.ShowBoard();
+
+            board.MovePiece(new Tuple<int, int>(0, 7), new Tuple<int, int>(2, 6));
+
+            board.ShowBoard();
+
+            board.MovePiece(new Tuple<int, int>(0, 7), new Tuple<int, int>(2, 3));
+
+            board.ShowBoard();
+        }
+
+        private static void ParticipantWork()
+        {
+            // add code here
+        }
+
+        private static void ProgramLoop(IChessBoard board)
+        {
+            Console.WriteLine("To move select your chess piece by command 'select x,y' where x is column and y is row.");
+            Console.WriteLine("Next move by command 'move x,y' where x is column and y is row.");
+            Console.WriteLine("Move:");
+            input = string.Empty;
+
+            while (!input.Equals("exit"))
+            {
+                input = Console.ReadLine().Trim().ToLower();
+
+                if (input.Contains("move"))
+                {
+                    if (selected == null)
+                    {
+                        Console.WriteLine("No chess piece selected.");
+                        continue;
+                    }
+
+                    if(!board.MovePiece(selected.Position, GetCoordinates(input)))
+                    {
+                        Console.WriteLine("Invalid move.");
+                    }
+
+                    board.ShowBoard();
+                    selected = null;
+                    continue;
+                }
+                else if (input.Contains("select"))
+                {
+                    try
+                    {
+                        selected = board.GetSquare(GetCoordinates(input));
+                        if(selected == null)
+                        {
+                            Console.WriteLine("No chess piece selected.");
+                        }
+                    }
+                    catch(ArgumentException e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+
+                    continue;
+                }
+
+                if (input.Contains("exit"))
+                {
+                    break;
+                }
+
+                Console.WriteLine("Invalid command. Try again.");
+            }
+        }
+
+        private static Tuple<int, int> GetCoordinates(string input)
+        {
+            char[] parameters = { ' ', ',' };
+            string[] splited = input.Split(parameters);
+            if(splited.Length != 3)
+            {
+                return new Tuple<int, int>(-1, -1);
+            }
+
+            int x = int.Parse(splited[1]);
+            int y = int.Parse(splited[2]);
+
+            return new Tuple<int, int>(x, y);
+        }
+    }
+}
---
+++ UnitTests/MovingUnitTest.cs
@@ -0,0 +1,43 @@
+﻿namespace UnitTestWorkshop.UnitTests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+    using UnitTestWorkshop.MajorChessPieces;
+
+    [TestClass]
+    public class MovingUnitTest
+    {
+        [TestMethod]
+        public void KnightProperMoveTest()
+        {
+            MockRepository repository = new MockRepository(MockBehavior.Loose);
+
+            // manual
+            var knightMock = repository.Create<IChessPiece>();
+            knightMock.SetupGet(c => c.Color).Returns(Utils.ColorsEnum.Black);
+            knightMock.Setup(m => m.Move(new System.Tuple<int, int>(2, 6))).Returns(true);
+            knightMock.SetupGet(p => p.Position).Returns(new System.Tuple<int, int>(2, 6));
+
+            var knight = knightMock.Object;
+
+            Assert.AreEqual(new System.Tuple<int, int>(2, 6), knight.Position);
+            Assert.IsTrue(knight.Move(new System.Tuple<int, int>(2, 6)));    
+        }
+
+        [TestMethod]
+        public void KnightWrongMoveTest()
+        {
+            MockRepository repository = new MockRepository(MockBehavior.Loose);
+
+            var knightMock = repository.Create<IChessPiece>();
+            knightMock.SetupAllProperties();
+            knightMock.SetupGet(p => p.Position).Returns(new System.Tuple<int, int>(0, 7));
+
+            var knight = knightMock.Object;
+
+            Assert.IsFalse(knight.Move(null));
+
+            Assert.AreEqual(new System.Tuple<int, int>(0, 7), knight.Position);
+        }
+    }
+}
